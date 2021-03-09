613d93f2735968f6e8085c1298ad30a7b36b679d
ChristeenaJose <45913504+ChristeenaJose@users.noreply.github.com>
2021-03-09 07:05:04 +0100

    Update validators.js
    Update: Issue Fixed
    1/ A special character cannot appear as the first or last character in an Local part of email address

--- src/validation/validators.js
+++ src/validation/validators.js
@@ -1,6 +1,6 @@
 function emailValidator () {
     return function email (value) {
-      return (value && !!value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) || 'Please enter a valid email'
+      return (value && !!value.match(/^[a-zA-Z0-9](([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))[a-zA-Z0-9]@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) || 'Please enter a valid email'
     }
   }
   
